049e62110306bd3a982015a740dcbc3523cdbff0
Tauseef Akhtar <89749134+Presidnttsf@users.noreply.github.com>
2026-08-02 11:59:57 +0530

    Implement system information display in system.py
    Added various system information print statements to system.py.

---
+++ python/system.py
@@ -0,0 +1,16 @@
+import sys
+print(sys.getrecursionlimit())
+print(sys.version)
+print(sys.version_info)
+print(sys.platform)
+print(sys.maxsize)
+
+
+import sys
+
+print(sys.maxsize)
+
+x = 9999999999999999999999999999999999999999999999999999
+print(x)
+
+#research more methods to explore python system
