82f145b2081164d5398bd276aa500392f11b77db
Tauseef Akhtar <89749134+Presidnttsf@users.noreply.github.com>
2026-08-10 20:51:39 +0530

    Create printFvsln.java to calculate total cost
    Added a Java program to calculate and print the sum of item price and shipping cost using both println and printf.

---
+++ java/printFvsln.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    // Write itemPrice here
+     int itemPrice = 40;
+     
+    // Write shippingCost here
+     int shippingCost = 15; 
+    // Write sum here (itemPrice + shippingCost)
+       int sum = itemPrice + shippingCost;
+
+
+    // Print sum here with println
+          System.out.println("Sum of shiping and item price = " + sum);
+     // print with printf
+     
+        System.out.printf("Sum of shipping and item price is %d", sum);
+  }
+}
