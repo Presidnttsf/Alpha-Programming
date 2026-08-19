dfbeec2ae342f25ce895d4c9b786615861edf8d4
Tauseef Akhtar <89749134+Presidnttsf@users.noreply.github.com>
2026-08-19 21:00:08 +0530

    Implement pyramid pattern printing in main method
    Added a pyramid pattern printing logic to the main method.

--- java/pyramid.java
+++ java/pyramid.java
@@ -6,6 +6,22 @@ public class main{
 
   public static void main(String [] args){
       
+      int num = 7;
+
+    for(int i=1; i<=num; i++){
+
+      for(int j=1; j<=num-i; j++){
+        System.out.print(" ");
+      
+    }
+      for(int k=1; k<=(2*i)-1; k++){
+        System.out.print("*");
+      
+    }
+      System.out.print("\n");
+    }
+
+
     
   }
 }
